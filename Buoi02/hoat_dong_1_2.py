11ed3cf7972838ab409d62f858f8a8ed15e54bcf
hatuan13122006 <hatuan13122006@gmail.com>
2026-09-11 09:42:22 +0700

    Hoan Thanh Buoi 04

--- Buoi02/hoat_dong_1_2.py
+++ Buoi02/hoat_dong_1_2.py
@@ -60,28 +60,3 @@ s6 = "Toi ten la \"Nam\", con ban ten gi?"
 print(s1); print(s2); print(s3); print(s4); print(s5); print(s6)
 
 
-so_nguyen = 15
-so_thuc = 4.2
-so_phuc = 3 + 4j
-print(type(so_nguyen), type(so_thuc), type(so_phuc))
-print(float(so_nguyen)) # ep int -> float
-print(int(so_thuc)) # ep float -> int (cat phan thap phan)
-
-
-a = -7
-b = 2.6789
-c, d = 17, 5
-print(abs(a)) # gia tri tuyet doi
-print(round(b)) # lam tron
-print(round(b, 2)) # lam tron 2 chu so thap phan
-print(pow(c, 2)) # c mu 2
-print(divmod(c, d)) # tra ve (thuong, du) dang tuple
-
-
-import math
-a, b, c = 1, -3, 2
-delta = b ** 2 - 4 * a * c
-x1 = (-b + math.sqrt(delta)) / (2 * a)
-x2 = (-b - math.sqrt(delta)) / (2 * a)
-print(f"Delta = {delta}")
-print(f"Nghiem x1 = {round(x1, 2)}, x2 = {round(x2, 2)}")
